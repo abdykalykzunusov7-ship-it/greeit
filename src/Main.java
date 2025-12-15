75ba3bc96729a0c69f056b636b4e7079ff30574b
Adik <abdykalykzunusov7@gmail.com>
2025-12-15 09:47:44 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] name;
+        String[] names;
+         name = new int[]{3,5,5,654,3,};
+         names = new String[]{"ivan","bain","evgen","adik"};
+     //   System.out.println(names[3]);
+       //     int ff = names.length;
+        //System.out.println(ff);
+        for (int i = 0; i < names.length; i ++){
+            System.out.println(names[i]);
+
+
+        }
+    for (int n: name){
+            System.out.println(n);
+        }
+      }
+
+    }
+
+
+
+
+
+
+
